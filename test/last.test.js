c4a4a0f562c30a2aee12309bacd883e41e5393b4
Soya Sano <soyasano2180plus1@gmail.com>
2018-02-06 21:21:02 +0900

    add test to last method

--- test/last.test.js
+++ test/last.test.js
@@ -12,4 +12,12 @@ describe('last()', () => {
   it('returns the whole array if you ask for more elements than it has', () => {
     expect(_.last(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
   });
+  
+  it('should be ruturn empty array', ()=>{
+    expect(_.last(['a', 'b', 'c'], -1)).toEqual([]);
+  });
+  
+  it('ruturns undefined object', ()=>{
+    expect(_.last([])).toBeUndefined();
+  });
 });
